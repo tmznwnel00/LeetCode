c0932f13fea4a4e16919440056a8e0e0a9882724
wantbdrgn <65274009+tmznwnel00@users.noreply.github.com>
2023-04-09 23:37:30 +0900

    Time: 461 ms (5.01%), Space: 15 MB (7.27%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def partitionString(self, s: str) -> int:
+        temp = ""
+        answer = 0
+        for letter in s:
+            print(temp, letter)
+            if letter not in temp:
+                temp += letter
+            else:
+                answer += 1
+                temp = letter
+        return answer if temp == "" else answer + 1
