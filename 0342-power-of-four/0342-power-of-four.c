2747fdfde1140f9cae84ad1ead233e02daef77eb
wantbdrgn <65274009+tmznwnel00@users.noreply.github.com>
2025-08-16 00:00:43 +0900

    Time: 0 ms (100%), Space: 7.8 MB (27.53%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.c
@@ -0,0 +1,12 @@
+bool isPowerOfFour(int n) {
+    if (n <= 0) return false;
+
+    while (n > 1) {
+        if (n % 4 != 0) {
+            return false;
+        } else {
+            n /= 4;
+        }
+    }
+    return true;
+}
